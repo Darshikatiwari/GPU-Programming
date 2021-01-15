488e6c196a66aee3231a94e8e044df5a95b34038
Darshika_Tiwari <69679393+Darshikatiwari@users.noreply.github.com>
2021-01-15 15:54:29 +0530

    Update multi_thread.h

--- header/multi_thread.h
+++ header/multi_thread.h
@@ -7,7 +7,7 @@ using namespace std;
 #define TIME_NOW std::chrono::high_resolution_clock::now()
 #define TIME_DIFF(gran, start, end) std::chrono::duration_cast<gran>(end - start).count()
 
-#define THREADS 64
+#define THREADS 48
 pthread_mutex_t alock;
 pthread_mutex_t block;
 int step =0;
